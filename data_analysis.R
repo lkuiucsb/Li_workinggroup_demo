bf2af4f0e7667360cd0b5d603dc2c6a88b61fc59
lkuiucsb <lkui@ucsb.edu>
2025-12-09 13:10:48 -0800

    change to data analysis code

---
+++ data_analysis.R
@@ -0,0 +1,21 @@
+# data analysis script
+
+# load dataset from the base R
+data("mtcars")
+
+# view the structure of the dataset
+str(mtcars)
+
+# summary statistics of the dataset
+summary(mtcars)
+
+#anova to see if there is a significant difference in mpg based on the number of cylinders
+anova_result <- lm(mpg ~ as.factor(cyl), data = mtcars)
+summary(anova_result)
+
+#plot the boxplot for mpg and cyl
+boxplot(mtcars$mpg ~ mtcars$cyl,
+        main = "Boxplot of MPG by Cylinder Count",
+        xlab = "Number of Cylinders",
+        ylab = "Miles Per Gallon (MPG)",
+        col = c("lightblue", "lightgreen", "lightpink"))
